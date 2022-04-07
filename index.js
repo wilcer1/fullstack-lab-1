0ba8e5d0fb9fe2ab0643530b632d03d0f7e2e6c3
wilcer1 <wille.cervin@live.se>
2022-04-07 20:10:53 +0200

    weqeqwewqeqw

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 dotenv.config();
 
 mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
-    console.log('connected to db');
+    console.log('connected to db'); 
 });
 app.use(express.json());
 app.use(express.static('public'));
--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ async function setID(){
        id++;
     }
     return id;
-}
+} 
 
 
 router.get("/users", async (req, res) => {
@@ -24,7 +24,7 @@ router.post("/user", async (req, res) => {
         id: await setID(),
         name: req.body.name,
         email: req.body.email
-    });
+    }); 
     await user.save();
     res.send(user);
 }catch{
@@ -36,8 +36,14 @@ router.post("/user", async (req, res) => {
 router.get("/users/:id", async (req, res) => {
     try {
     const user = await User.findOne({id: req.params.id});
+    
+    if(user == null){
+        res.status(404);
+        res.send({ error: "user doesn't exist!" });
+    }
     res.send(user);
-} catch {
+
+} catch (error){
     res.status(404);
     res.send({ error: "user doesn't exist!" });
     }
@@ -46,7 +52,7 @@ router.get("/users/:id", async (req, res) => {
 router.patch("/users/:id", async (req, res) => {
     try {
     const user = await User.findOne({ id: req.params.id });
-    console.log(req.header("name"), req.header("email"));
+
     user.name = req.header("name");
     
     user.email = req.header("email");
@@ -62,8 +68,9 @@ router.patch("/users/:id", async (req, res) => {
 
 router.delete("/users/:id", async (req, res) => {
     try {
-    await User.deleteOne({ id: req.params.id })
-    res.status(204).send();
+    await User.deleteOne({ id: req.params.id });
+    res.status(200);
+    res.send({succes: `User with id ${req.params.id} was deleted successfully`});
     } catch {
     res.status(404);
     res.send({ error: "User doesn't exist!" })
